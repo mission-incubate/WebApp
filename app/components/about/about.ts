58bac7e595bef730e903b83b326ff05fc4be6c02
Natarajan Ganapathi <natarajanmca11@gmail.com>
2015-10-02 13:11:09 -0700

    Angular2 seed

---
+++ app/components/about/about.ts
@@ -0,0 +1,20 @@
+import {Component, View, CORE_DIRECTIVES} from 'angular2/angular2';
+
+import {NameList} from '../../services/name_list';
+
+@Component({
+  selector: 'about'
+})
+@View({
+  templateUrl: './components/about/about.html',
+  directives: [CORE_DIRECTIVES]
+})
+export class About {
+  constructor(public list: NameList) {}
+  addName(newname):boolean {
+    this.list.add(newname.value);
+    newname.value = '';
+    // prevent default form submit behavior to refresh the page
+    return false;
+  }
+}
