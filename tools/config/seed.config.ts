17866e4d5e6f0f563973b8c18264e2d4f1eb7428
Natarajan Ganapathi <natarajanmca11@gmail.com>
2016-03-19 02:27:19 +1300

    bootstrap-Material-Design included.

--- tools/config/seed.config.ts
+++ tools/config/seed.config.ts
@@ -61,7 +61,8 @@ export class SeedConfig {
     { src: 'rxjs/bundles/Rx.js', inject: 'libs' },
     { src: 'angular2/bundles/angular2.js', inject: 'libs' },
     { src: 'angular2/bundles/router.js', inject: 'libs' },
-    { src: 'angular2/bundles/http.js', inject: 'libs' }
+    { src: 'angular2/bundles/http.js', inject: 'libs' },
+    { src: 'bootstrap-material-design/css/bootstrap-material-design.css', inject: true }
   ]);
 
   PROD_NPM_DEPENDENCIES: InjectableDependency[] = normalizeDependencies([
@@ -69,7 +70,8 @@ export class SeedConfig {
     { src: 'reflect-metadata/Reflect.js', inject: 'shims' },
     { src: 'es6-shim/es6-shim.min.js', inject: 'shims' },
     { src: 'systemjs/dist/system.js', inject: 'shims' },
-    { src: 'angular2/bundles/angular2-polyfills.min.js', inject: 'libs' }
+    { src: 'angular2/bundles/angular2-polyfills.min.js', inject: 'libs' },
+    { src: 'bootstrap-material-design/css/bootstrap-material-design.css', inject: true }
   ]);
 
   // Declare local files that needs to be injected
--- tools/tasks/project/build.sass.src.ts
+++ tools/tasks/project/build.sass.src.ts
@@ -1,11 +1,16 @@
 import * as gulp from 'gulp';
 import * as gulpLoadPlugins from 'gulp-load-plugins';
 import {join} from 'path';
-import {APP_SRC} from '../../config';
+import {APP_SRC, ENV} from '../../config';
 const plugins = <any>gulpLoadPlugins();
 
+const isProd = ENV === 'prod';
+
 export = () => {
-  return gulp.src(join(APP_SRC, '**', '*.scss'))
-    .pipe(plugins.sass().on('error', plugins.sass.logError))
-    .pipe(gulp.dest(APP_SRC));
+    let src = [join(APP_SRC, '**', '*.scss')];
+    return gulp.src(src)
+        .pipe(isProd ? plugins.util.noop() : plugins.sourcemaps.init())
+        .pipe(plugins.sass().on('error', plugins.sass.logError))
+        .pipe(isProd ? plugins.util.noop() : plugins.sourcemaps.write()) //map written inline
+        .pipe(gulp.dest(APP_SRC));
 }
